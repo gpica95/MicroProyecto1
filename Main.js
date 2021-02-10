92f886128db4757bada553867f022b2cee929750
Gabriel <gabrielpica@correo.unimet.edu.ve>
2021-02-09 22:51:15 -0400

    Cambios en header y carrousel

---
+++ Main.js
@@ -0,0 +1,37 @@
+var imagenes = ['img/mountain1.jpg', 'img/mountain2.jpg', 'img/mountain3.jpg', 'img/mountain4.jpg'],
+cont = 0;
+
+function carrousel(contenedor){
+    contenedor.addEventListener('click', e => {
+        let atras = contenedor.querySelector('.atras'),
+            adelante = contenedor.querySelector('.adelante'),
+            img = contenedor.querySelector('img'),
+            tgt = e.target;
+
+
+        if(tgt == atras){
+            if(cont > 0){
+                img.src = imagenes[cont - 1];
+                cont--;
+            }else {
+                img.src = imagenes[imagenes.length - 1 ];
+                cont = imagenes.length -1;
+            }
+        }else if(tgt == adelante){
+            if(cont < imagenes.length - 1){
+                img.src = imagenes[cont + 1];
+                cont++;
+            }else {
+                img.src = imagenes[0];
+                cont = 0;
+            }
+        }
+
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    let contenedor = document.querySelector('.contenedor');
+
+    carrousel(contenedor);
+})
